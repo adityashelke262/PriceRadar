a13924585eb40fe6a46f616da9c7a2319c63075d
Aditya Shelke <adityashelke262@gmail.com>
2026-07-03 17:53:36 +0530

    Improved hero, search page responsiveness and UI

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,9 +4,11 @@ import { FiHeart } from "react-icons/fi";
 import { FiArrowRight } from "react-icons/fi";
 import logo from "../../assets/a.png";
 import React, { useEffect, useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,44 +20,83 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <nav className={`navbar ${scrolled ? "navbar-scrolled" : ""}`}>
-      {/* LEFT */}
+   <nav className={`navbar ${scrolled ? "navbar-scrolled" : ""}`}>
 
-      <Link to="/" className="navbar-logo">
-        <img src={logo} alt="PriceRadar" />
+  <div className="navbar-container">
 
-        <div>
-          <h2>PriceRadar</h2>
+    {/* LEFT */}
 
-          <p>Compare • Save • Buy</p>
-        </div>
-      </Link>
+    <Link to="/" className="navbar-logo">
+      <img src={logo} alt="PriceRadar" />
 
-      {/* CENTER */}
-
-      <div className="navbar-links">
-        <NavLink to="/">Home</NavLink>
-
-        <NavLink to="/search">Search</NavLink>
-
-        <NavLink to="/compare">Compare</NavLink>
+      <div>
+        <h2>PriceRadar</h2>
+        <p>Compare • Save • Buy</p>
       </div>
+    </Link>
 
-      {/* RIGHT */}
+    {/* Desktop Links */}
 
-      <div className="navbar-right">
-        <button className="login-btn">
-          Login
-          <FiArrowRight />
-        </button>
+    <div className="navbar-links">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/search">Search</NavLink>
+      <NavLink to="/compare">Compare</NavLink>
+    </div>
 
-        <button className="wishlist-btn">
-          <FiHeart />
+    {/* Desktop Right */}
 
-          <span>Wishlist</span>
-        </button>
-      </div>
-    </nav>
+    <div className="navbar-right">
+
+      <button className="login-btn">
+        Login
+        <FiArrowRight />
+      </button>
+
+      <button className="wishlist-btn">
+        <FiHeart />
+        <span>Wishlist</span>
+      </button>
+
+    </div>
+
+    {/* Mobile Menu Icon */}
+
+    <div
+      className="menu-icon"
+      onClick={() => setMenuOpen(!menuOpen)}
+    >
+      {menuOpen ? <FiX /> : <FiMenu />}
+    </div>
+
+  </div>
+
+  {/* Mobile Drawer */}
+
+  <div className={`mobile-menu ${menuOpen ? "show-menu" : ""}`}>
+
+    <NavLink to="/" onClick={() => setMenuOpen(false)}>
+      Home
+    </NavLink>
+
+    <NavLink to="/search" onClick={() => setMenuOpen(false)}>
+      Search
+    </NavLink>
+
+    <NavLink to="/compare" onClick={() => setMenuOpen(false)}>
+      Compare
+    </NavLink>
+
+    <button className="mobile-login">
+      Login
+    </button>
+
+    <button className="mobile-wishlist">
+      Wishlist
+    </button>
+
+  </div>
+
+</nav>
   );
 };
 
